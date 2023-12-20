97382bb6bee452b3792db69287f27dd3c0a22973
Rogerio pc Interativa <rogerioo.lara@hotmail.com>
2023-12-20 17:49:52 -0300

    controller e rota de criação de tasks implementado

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -12,7 +14,11 @@ class TaskController extends Controller
 
     public function create(Request $request)
     {
-        return view('tasks.create');
+        $categories = Category::all();
+
+        $data['categories'] = $categories;
+
+        return view('tasks.create', $data);
     }
 
     public function edit(Request $request)
@@ -22,6 +28,15 @@ class TaskController extends Controller
     public function delete(Request $request)
     {
         //deleta e redireciona de volta para a home
+        return redirect(route('home'));
+    }
+
+    public function create_action(Request $request)
+    {
+        $task = $request->only(['title', 'category_id', 'description', 'due_date']);
+        // completando os dados para gravar no banco (gambiarra)
+        $task['user_id'] = 1;
+        $dbTask = Task::create($task);
         return redirect(route('home'));
     }
 }
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -9,18 +9,21 @@
     <section id="create_task_section">
         <h1>Criar tarefa</h1>
 
-        <form action="">
+        <form method="POST" action="{{ route('task.create_action') }}">
+            @csrf
 
             <x-form.text_input name="title" label="Título da tarefa" required="required"
                 placeholder="Digite o título da tarefa" />
 
             <x-form.text_input name="due_date" required="required" type="date" label="Data de realização" />
 
-            <x-form.select_input name="category" label="Categoria" placeholder="">
-                <option>Valor qualquer</option>
+            <x-form.select_input name="category_id" label="Categoria" placeholder="">
+                @foreach ($categories as $category)
+                    <option value="{{ $category->id }}">{{ $category->title }}</option>
+                @endforeach
             </x-form.select_input>
 
-            <x-form.textarea_input name="description" placeholder="Digite a descrição da tarefa"
+            <x-form.textarea_input name="description" placeholder="Digite a descrição da tarefa" required
                 label="Descrição da tarefa" />
 
             <x-form.form_btn resetTxt="Resetar" submitTxt="Criar Tarefa" />
